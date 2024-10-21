3e2ea617e860f47774cc98d537a53ba3d2e3c36e
MANOHARA <126142297+manu41608@users.noreply.github.com>
2024-10-22 01:15:39 +0530

    Update move-file.sh

--- move-file.sh
+++ move-file.sh
@@ -24,9 +24,9 @@ for name in "${name_array[@]}"; do
         echo "File /tmp/$name does not exist, skipping."
         continue
     fi
-    # if [ "$VM" == "1" ]; then
-    #     sudo scp /tmp/$name "$USRNAME@$VM2:/tmp"
-    # fi
+    if [ "$VM" == "1" ]; then
+        sudo scp "/tmp/$name" "$USRNAME@$VM2:/tmp"
+    fi
     # Set permissions and ownership
     sudo chmod 755 "/tmp/$name"
     sudo chown root:root "/tmp/$name"
